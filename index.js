69a7f00e58b64d5756a2fc9f6118cb5991c22894
anna-sahradyan <a.sahradyan72@gmail.com>
2023-06-26 15:10:08 +0400

    deleted env file

--- index.js
+++ index.js
@@ -1,27 +1,17 @@
 const express = require("express");
-const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 const dotenv = require("dotenv");
-const path = require("path");
 dotenv.config();
 const cors = require("cors");
 const connectDB = require("./config/connectDB");
 const bodyParser = require("body-parser");
-
+const app = express();
 app.use(bodyParser.json());
-
-
-app.get("/", (req, res) => {
-    return res.send("Hello World !!!")
-});
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
 //?DEPLOYMENT CODE
-app.get("/", (req, res) => {
-    res.send("Home Page")
-});
 
 connectDB();
 app.listen(PORT, () => {
