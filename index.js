256a082600ca10733232bf6575555712d239bf87
anna-sahradyan <a.sahradyan72@gmail.com>
2023-06-27 14:03:11 +0400

    added new files user model and user router

--- index.js
+++ index.js
@@ -5,15 +5,28 @@ dotenv.config();
 const cors = require("cors");
 const connectDB = require("./config/connectDB");
 const bodyParser = require("body-parser");
+const userRouter = require("./routes/user-router");
 const app = express();
+
+
+//?middlewares
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors(
+    {origin: true}
+));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-
+//?Router middleware
+app.use("/api/users", userRouter);
 //?DEPLOYMENT CODE
+app.get("/", (req, res) => {
+    res.send("Home page")
+})
+
 
 connectDB();
+
+
 app.listen(PORT, () => {
     console.log(` Server is running in http://localhost:${PORT}`)
 })
