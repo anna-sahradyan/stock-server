2957cf62743702957de2e90c7292f1a21c075b32
anna-sahradyan <a.sahradyan72@gmail.com>
2023-06-30 18:39:32 +0400

    added new middleware errorMiddleware

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,3 +1,22 @@
-export const registerUser = async () => {
+//import User from "../models/User";
 
+const registerUser = async (req, res, next) => {
+    try {
+        if (!req.body.email) {
+             res.status(400);
+            throw new Error("Please add an  email");
+        } else {
+            res.send("hello register");
+        }
+    } catch (error) {
+        next(error);
+    }
+};
+
+
+
+
+
+module.exports = {
+    registerUser
 }
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const PORT = process.env.PORT || 5000;
 const dotenv = require("dotenv");
+const errorHandler = require("./middleware/errorMiddleware");
 dotenv.config();
 const cors = require("cors");
 const connectDB = require("./config/connectDB");
@@ -11,18 +12,20 @@ const app = express();
 
 //?middlewares
 app.use(bodyParser.json());
-app.use(cors(
-    {origin: true}
-));
 app.use(express.json());
+app.use(cors({origin: true}));
 app.use(express.urlencoded({extended: false}));
+
+
+
 //?Router middleware
 app.use("/api/users", userRouter);
 //?DEPLOYMENT CODE
 app.get("/", (req, res) => {
     res.send("Home page")
 })
-
+//?errorMiddleware
+app.use(errorHandler);
 
 connectDB();
 
